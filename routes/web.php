fcdf43dbe48edecab1f6072fd2281b80719fc8a8
xBlackTea <xBlackTea.Official@gmail.com>
2024-08-07 02:06:57 +0900

    add : station2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\PracticeController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('practice', function() {
+//     return response('practice');
+// });
+
+// Route::get('practice2', function() {
+//     $test = 'practice2';
+// return response($test);
+// });
+
+// Route::get('practice3', function() {
+//     $test = 'test';
+// return response($test);
+// });
+
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
